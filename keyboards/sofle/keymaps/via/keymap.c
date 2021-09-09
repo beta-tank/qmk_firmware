863aa67161c12e9e49176f54094c6dd35c061793
beta-tank <beta-tank@outlook.com>
2021-09-09 21:20:34 +0700

    Jian-like dirty layout

--- keyboards/sofle/keymaps/via/keymap.c
+++ keyboards/sofle/keymaps/via/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_RBRC,
+  KC_LGUI,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     RGUI_T(KC_RBRC),
   KC_GRV,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  RCTL_T(KC_QUOT),
   KC_LALT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,               XXXXXXX,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  RALT_T(KC_BSLS),
@@ -70,10 +70,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *           `------------------------------------'           '-------''------------------------------'
  */
 [_LOWER] = LAYOUT(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                           KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, RCTL_T(KC_PIPE),
-  _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_RALT,
+  _______, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______,  RGUI_T(KC_F12),
+  KC_UNDS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  LCTL_T(KC_EQL),  KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, RCTL_T(KC_MINS),
+  LALT_T(KC_PPLS), KC_1,    KC_2,  KC_3,    KC_4,    KC_5,  _______,                   _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
                     _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 /* RAISE
@@ -91,15 +91,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *           `------------------------------------'           '-------''------------------------------'
  */
 [_RAISE] = LAYOUT(
-  KC_ESC,  _______ , _______ , _______ , _______ , _______,                              _______,  _______ , _______,  _______ ,  _______ ,_______,
-  _______, KC_INS,   KC_PSCR,  KC_APP,  XXXXXXX,  XXXXXXX,                               KC_VOLU, KC_HOME,  KC_UP,     KC_PGUP,   KC_SLCK, KC_CAPS,
-  _______, KC_LALT,  KC_LCTL,  KC_LSFT, XXXXXXX,  KC_CAPS,                               KC_MUTE, KC_LEFT,  KC_DOWN,   KC_RGHT,   KC_INS,  RCTL_T(KC_APP),
-  _______, KC_UNDO,  KC_CUT,   KC_COPY, KC_PASTE, TG(_GAMING),   _______,       _______, KC_VOLD, KC_END,   KC_PSCR,   KC_PGDN,   KC_PAUS, _______,
+  _______ ,  _______ , _______ , _______ , _______ , _______,                              _______,  _______ , _______,  _______ ,  _______ ,_______,
+  KC_NLCK, KC_PSLS,  KC_P7,    KC_P8,   KC_P9,    KC_PMNS,                               KC_VOLU, KC_HOME, KC_UP, KC_PGUP, KC_MY_COMPUTER, KC_CAPS,
+  LCTL_T(KC_EQL),  KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,      KC_MUTE, KC_LEFT, KC_DOWN,   KC_RGHT, KC_APP,  RCTL_T(KC_INS),
+   _______,         KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PCMM, _______,    _______, KC_VOLD, KC_END,  KC_PSCR, KC_PGDN, KC_CALCULATOR, RALT_T(KC_SLCK),
                          _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______
 ),
 [_ADJUST] = LAYOUT_symmetric(
   _______, _______, _______, _______, _______, _______, 
-  RESET,   XXXXXXX, RGB_SPI,  RGB_VAI, RGB_HUI, RGB_SAI,
+  RESET,   TG(_GAMING), RGB_SPI,  RGB_VAI, RGB_HUI, RGB_SAI,
   DEBUG,   XXXXXXX, RGB_SPD,  RGB_VAD, RGB_HUD, RGB_SAD,
   RGB_MODE_GRADIENT, RGB_MODE_TWINKLE, RGB_MODE_PLAIN, RGB_TOG, RGB_RMOD,RGB_MOD, _______,
                     _______, _______, _______, _______, _______
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,      KC_1,      KC_2,    KC_5,    KC_6,    KC_4,                                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_RBRC,
   TG(_GAMING), KC_RBRC,   KC_LCTL, KC_W,    KC_SPC,  KC_R,                                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,
   KC_TAB,      KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,                                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  RCTL_T(KC_QUOT),
-  KC_V,        KC_LALT,   KC_Z,    KC_X,    KC_C,    KC_G, KC_MUTE,               XXXXXXX,     KC_F2,   KC_F11,  KC_COMM, KC_DOT,  KC_SLSH,  RALT_T(KC_BSLS),
+  KC_V,        KC_LALT,   KC_Z,    KC_X,    KC_C,    KC_G, LCTL(KC_M),               KC_MUTE,     KC_F2,   KC_F11,  KC_COMM, KC_DOT,  KC_SLSH,  RALT_T(KC_BSLS),
                           KC_6,    KC_5,    KC_Q,    KC_E, KC_4,                  TG(_GAMING), KC_LSFT, KC_LALT, KC_RALT, KC_RGUI
 )
 };
